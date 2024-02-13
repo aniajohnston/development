6731761487d9aeeb84bdd05622edecbd3cb632a7
aniajohnston <ajohns81@syr.edu>
2024-02-13 17:32:13 -0500

    prep before second class

---
+++ class1/helloworld.py
@@ -0,0 +1,45 @@
+# brings in panda library pd is shorthand for pandas
+
+import pandas as pd
+
+
+
+print("hello world")
+print('hello world')
+# print(hello) 
+
+# hit the quotes 3 times
+'''
+all of this is commented out
+'''
+
+# assigning a variable to a string
+
+hello = "hello world"
+print(hello)
+
+'''
+EQUATION OF PERIMETER
+2(length+width)
+'''
+
+length = 450
+
+width = 52
+
+perimeter = 2*(450+52)
+
+print(perimeter)
+
+'''
+C = (F-32)*5/9
+'''
+
+F = 76
+
+C = (F-32)*5/9
+
+print(C)
+
+# a data frame/ library called Pandas
+data = {'salespeople':['Sam','Reynelle','Michael'],'sales':[150, 125, 115]}
