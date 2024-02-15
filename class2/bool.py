17e3160a6c1408f1fb145027c57e2a206c5c849b
aniajohnston <ajohns81@syr.edu>
2024-02-14 20:43:23 -0500

    boolean operations

---
+++ class2/bool.py
@@ -0,0 +1,30 @@
+'''
+# You are given a triangle with a side #1 of 4, base of 6, and side #2 of 3. Create
+a brief python script that will determine the perimeter of the triangle. Comment your code
+
+1. Print the perimeter
+2. Using boolean operators is side #1 greater than the base?
+3. Using boolean operators is side #2 less than the base?
+4. Using boolean operators is base larger than or equal to side #1?
+
+'''
+# Triangle of side one 4 base of 6 side 2 of 3 
+#perimeter of a triangle is length + base + height
+
+side_one = 4 #variables cannot start with numbers
+base = 6
+side_two = 7
+
+perimeter_triangle = side_one + base + side_two
+
+print(perimeter_triangle)
+print(side_one > base)
+print(side_two < base)
+print(base >= side_one)
+
+
+
+
+
+
+
