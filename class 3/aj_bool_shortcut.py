0f77ae953f39214bf100171e4d724538d5ccb1ec
aniajohnston <ajohns81@syr.edu>
2024-02-19 19:11:48 -0500

    class 3 new file

---
+++ class 3/aj_bool_shortcut.py
@@ -0,0 +1,41 @@
+# Boolean Operators
+
+# Is 4 less than or equal to 4? <=
+# result=(4 <= 4)
+# print(result)
+
+# Is 6 greater than 2? >
+# result =(6 > 10)
+# print(result)
+
+# Is 5 greater than or equal to 6? >=
+# result=( 5 >= 6)
+# print(result)
+
+# Is 5 equal to 5? ==
+# print(5 == 7)
+
+# Shortcut operators
+
+# Addition 
+# val = 8
+# val = val + 3
+# val += 3
+# print(val)
+
+# Subtraction 
+# var = 20
+# var -= 5
+# print(var)
+
+
+# Multiplication
+var = 5
+var *= 5
+print(var)
+
+
+num = 5
+num -= 2
+
+# print(num)
