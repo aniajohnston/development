db3dccd9fd65d0b98b1474536938a54fd9eb2656
aniajohnston <ajohns81@syr.edu>
2024-02-21 19:57:30 -0500

    introduction to pandas

---
+++ class 3/animals.py
@@ -0,0 +1,14 @@
+# api pulls data from survey software
+# the data gotten is called response.json 
+# pandas is similar this library will create a data frame from the json
+# we will get a list from this 
+
+import pandas as pd
+
+animals = ['Lions', 'Tigers', 'Bears', 'Dogs', 'Cats' ] #List collection
+
+df = pd.DataFrame(animals) #Convert our list to a format which Pandas can use
+
+print(df)
+
+df.to_csv('output.csv') #this will give us our output
